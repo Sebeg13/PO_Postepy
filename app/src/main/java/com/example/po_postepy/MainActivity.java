f4a3a77b397dd13035af12a115a04e03821f39d2
Sebastian <sebastian.lauks13@gmail.com>
2020-01-19 18:06:58 +0100

    added custom Action bar

--- app/src/main/java/com/example/po_postepy/MainActivity.java
+++ app/src/main/java/com/example/po_postepy/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.po_postepy;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 import android.view.View;
@@ -41,6 +42,8 @@ public class MainActivity extends AppCompatActivity implements PostepyView {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setSupportActionBar((Toolbar)findViewById(R.id.activity_main_toolbar));
 
 
         presenter = new PostepyPresenter(this);
